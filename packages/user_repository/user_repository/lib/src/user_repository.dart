1ded8ba366f37a1340cf9ecf7bc0c9d485433aab
Benjamin Shapiro <bshap93@gmail.com>
2023-02-27 17:41:07 -0700

    Add repositories

---
+++ packages/user_repository/user_repository/lib/src/user_repository.dart
@@ -0,0 +1,17 @@
+// TODO Implement this library.
+import 'dart:async';
+import 'package:uuid/uuid.dart';
+
+import 'package:user_repository/src/models/models.dart';
+
+class UserRepository {
+  /// User might be logged in or not
+  User? _user;
+
+  /// If a user is logged in then return it otherwise wait and create a user
+  Future<User?> getUser() async {
+    if (_user != null) return _user;
+    return Future.delayed(const Duration(milliseconds: 300),
+        () => _user = User(const Uuid().v4()));
+  }
+}
