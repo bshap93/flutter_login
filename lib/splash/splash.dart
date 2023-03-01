f8fe930b64bb747ed4d744499c63d997c4ee5b65
Benjamin Shapiro <bshap93@gmail.com>
2023-03-01 12:40:32 -0700

    Finish the ui and started testing

--- lib/splash/splash.dart
+++ lib/splash/splash.dart
@@ -1,1 +1,2 @@
 // TODO Implement this library.
+export 'view/splash_page.dart';
---
+++ test/authentication/authentication_state_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_login/authentication/authentication.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:user_repository/user_repository.dart';
+
+class MockUser extends Mock implements User {}
+
+void main() {
+  group('AuthenticationState', () {
+    group('AuthenticationState.unknown', () {
+      test('supports value comparisons', () {
+        expect(
+          AuthenticationState.unknown(),
+          AuthenticationState.unknown(),
+        );
+      });
+    });
+  });
+}
